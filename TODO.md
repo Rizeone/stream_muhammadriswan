a2de1eaaf398e9e6cbc3a8cd8bc116fa0e1eb6e7
Muhammad Riswan <riswan@example.com>
2026-05-04 17:48:39 +0800

    W12: Jawaban Soal 2

---
+++ TODO.md
@@ -0,0 +1,8 @@
+# TODO: Add colors list to ColorStream
+
+## Steps:
+1. [x] Create TODO.md with plan steps
+2. [x] Edit lib/stream.dart to add the colors list inside ColorStream class
+3. [x] Update TODO.md with completion
+4. [x] Test the app with `flutter run`
+5. [x] Complete task
---
+++ lib/stream.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ColorStream {
+  static final List<Color> colors = [
+    Colors.blueGrey,
+    Colors.amber,
+    Colors.deepPurple,
+    Colors.lightBlue,
+    Colors.teal,
+    Colors.red,
+    Colors.green,
+    Colors.orange,
+    Colors.pink,
+    Colors.indigo
+  ];
+}
